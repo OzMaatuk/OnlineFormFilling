8dffcfb9b2c8cef820915258f937b3dc10aaf312
Oz <ozmaatuk@gmail.com>
2025-02-16 23:30:54 +0000

    adapt main to form_fill changes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # main.py
 
+from time import sleep
 from dotenv import load_dotenv
 from llm_utils.manager import LLMUtils
 from playwright.sync_api import sync_playwright
@@ -8,7 +9,7 @@ from form_filling.form_filling import FormFilling
 def main():
     load_dotenv()
     llm = LLMUtils()
-    url = ""
+    url = "https://www.comeet.com/jobs/crossriver/C7.00F/would-love-to-join-cross-river/92.F23"
     resume_content = None
     resume_path = "data/personal/resume.pdf"
     
@@ -17,23 +18,25 @@ def main():
         page = browser.new_page()
         page.goto(url)
         
+        sleep(3)
+
         form_filling = FormFilling(llm, resume_content, resume_path)
         
         # Example of filling an input field
         input_element = page.query_selector("input[type='text']")
-        form_filling.fill_input(input_element)
+        form_filling.fill_element(input_element)
         
         # Example of filling a textarea
         textarea_element = page.query_selector("textarea")
-        form_filling.fill_textarea(textarea_element)
+        form_filling.fill_element(textarea_element)
         
         # Example of filling a select element
         select_element = page.query_selector("select")
-        form_filling.fill_select(select_element)
+        form_filling.fill_element(select_element)
         
         # Example of filling radio buttons
         radio_elements = page.query_selector_all("input[type='radio']")
-        form_filling.fill_radio(radio_elements[0], options=radio_elements)
+        form_filling.fill_element(radio_elements[0])
         
         # Example of handling file upload
         file_input_element = page.query_selector("input[type='file']")
