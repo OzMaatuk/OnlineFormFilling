e1d9f7356108faeebedec7ec4d6b7c077a1a6eed
Oz <ozmaatuk@gmail.com>
2025-02-22 21:12:12 +0200

    add known_types

--- form_filling/element_utils.py
+++ form_filling/element_utils.py
@@ -20,7 +20,7 @@ class ElementUtils:
             logger.debug(f"Input element type: {input_type}")
             return input_type
         
-        known_types = ["text", "select", "select-one", "textarea", "radio", "checkbox"]
+        known_types = ["text", "select", "select-one", "textarea", "radio", "checkbox", "a", "button"]
         if tag_name in known_types:
             logger.debug(f"Element has known tag type: {tag_name}")
             return tag_name        
